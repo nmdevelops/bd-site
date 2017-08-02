fa9dc2e23ea89af80f15755ca79684fa157ae550
Nathan Mayer <nm@gmail.com>
2017-08-02 16:54:34 -0700

    complete basic concept of site landing

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,29 @@
 // ||                             ||
 // =================================
 
+  var btnID;
+  var idToCollapse;
+  var viewH;
 
+var categoryExpand = (function() {
+  $("." + btnID).slideDown();
+  $("button#" + btnID).off("mouseover");
+  $("button#" + btnID).removeClass("coolBottom");
+  $("button#" + btnID).addClass("bd-disabled");
+  $("button#" + btnID).prop("disabled", true);
+  $("#expand-" + btnID).removeClass("glyphicon glyphicon-chevron-down");
+})
 
-
+var categoryCollapse = (function(idToCollapse) {
+  console.log("ID to Collapse -->  " + idToCollapse);
+  $("." + idToCollapse).slideUp();
+  console.log("after slideUp");
+  $("button#" + idToCollapse).on("mouseover", categoryExpand);
+  $("button#" + idToCollapse).removeClass("bd-disabled");
+  $("button#" + idToCollapse).addClass("coolBottom");
+  $("button#" + idToCollapse).prop("disabled", false);
+  $("#expand-" + idToCollapse).addClass("glyphicon glyphicon-chevron-down");
+})
 
 
 
@@ -26,10 +46,31 @@
 // =================================
 
 $(document).ready(function() {
-  $("button#whatYouDo").click(function(event) {
+
+
+//This function grabs the elements ID to correctly point the function
+  $("button").mouseenter(function() {
+    btnID = $(this).attr('id');
+  })
+
+//first instance only
+  $("button#whatYouDo").mouseover(function(event) {
     event.preventDefault();
-    $(".whatYouDo").toggle();
+    categoryExpand(btnID);
   });
+  $("button#whatYouNeed").mouseover(function(event) {
+    event.preventDefault();
+    categoryExpand(btnID);
+  });
+
+  //takes input from glyphicon and runs function
+  $(".collapse").click(function(event) {
+    event.preventDefault();
+    idToCollapse = this.dataset.group;
+    categoryCollapse(idToCollapse);
+  })
+
+  //Functions as expected
   $("button#whatYouNeed").click(function(event) {
     event.preventDefault();
     $(".whatYouNeed").toggle();
